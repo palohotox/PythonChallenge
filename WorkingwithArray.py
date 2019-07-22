84d72bc44c6cfcdcf7703ddb490ce2355c5ac8ef
AzrulZ <49754767+palohotox@users.noreply.github.com>
2019-07-22 08:35:29 +0800

    Add files via upload

---
+++ WorkingwithArray.py
@@ -0,0 +1,24 @@
+
+from array import *
+
+myarr = array('i',[1,2,3,5,6,7,2]) #array
+myarr2 = array('i',[7,4,5,7,8,9,6]) #array
+numlist = [5,5,5,5,5,1,1,2,2]   # List
+
+#print("Original: " , myarr)
+
+#myarr.append(1000) # Will append new element on the last loction
+#myarr.pop(0)  # Will remove third element
+#myarr.remove(2)  # Will remove first occurence od 2 in the array.
+#myarr3 = myarr + myarr2 # Will append the 2nd array next to the first one
+#myarr3 = myarr - myarr2 # Unsupported
+#myarr3 = myarr * myarr2 # Unsupported
+#Insert new item at specific location in the array
+#myarr.insert(len(myarr),1000)
+#print("Count number of occurences of any elements: " , myarr.count(2))
+#myarr.reverse()  # reverse element in the array
+#myarr.extend(myarr2) #concatenate or extend anyarray to orig array
+#myarr.fromlist(numlist) #append an array from specified list
+#mynewlist = myarr.tolist() #Convert array to ordinary list
+
+print("Output: " , mynewlist)
